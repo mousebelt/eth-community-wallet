828853df3854190dcf2033a7b1625b9856950b79
crazydev71 <crazy.dev@aol.com>
2018-06-29 05:42:23 -0700

    Update icons with nrl icon

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,6 +1,44 @@
 import { injectGlobal } from 'styled-components';
 
 /* eslint no-unused-expressions: 0 */
+/* injectGlobal`
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+  }
+
+  body {
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  body.fontLoaded {
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  }
+
+  #app {
+    background-color: #f8f8f8;
+    min-height: 100%;
+    min-width: 100%;
+  }
+
+  p,
+  label {
+    font-family: 'Space' Georgia, Times, 'Times New Roman', serif;
+    line-height: 1.5em;
+  }
+
+  body {
+    text-align: center;
+  }
+  hr {
+    width: 200px;
+    text-align:center !important;
+    margin: 0 auto !important;
+  }
+`; */
+
+/* eslint no-unused-expressions: 0 */
 injectGlobal`
   html,
   body {
@@ -24,7 +62,7 @@ injectGlobal`
 
   p,
   label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
+    font-family: 'Space' Georgia, Times, 'Times New Roman', serif;
     line-height: 1.5em;
   }
 
